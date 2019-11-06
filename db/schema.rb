26c61ee9fd99b2cc49ff3e7e4045d1e489383780
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-06 22:31:35 +0900

    migrate db #4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_131760) do
+ActiveRecord::Schema.define(version: 2019_11_06_133055) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2019_11_06_131760) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "topics", force: :cascade do |t|
