8870ccf21d8c6ba6b3eae5b30c16c62800d26b5b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-09 14:50:03 +0900

    add search page in bbs #6

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def search
+    @titles = Topic.ransack(:title_cont => params[:keyword]).result
+    @contents = ActionText::RichText.ransack(:content_cont => params[:keyword]).result
+    @comments = Comment.ransack(:content_cont => params[:keyword]).result
   end
 end
